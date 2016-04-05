b3c1290c8da25bdd393741574a570833fa9a1686
mikehajj <mike.hajj@gmail.com>
2016-04-05 15:54:16 +0300

    added config.js for controller with deployer information.

---
+++ config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+	type: 'service',
+	prerequisites: {
+		cpu: '',
+		memory: ''
+	},
+	serviceVersion: 1,
+	serviceName: "controller",
+	serviceGroup: "SOAJS Core Service",
+	servicePort: 4000
+};
