ed3d0066fccc354bbbe490daba54933138bfea6a
antoinehage <me@antoinehage.com>
2019-03-26 08:45:56 -0400

    added dependencies added first unit test

---
+++ test/unit/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+describe("Starting Gateway Unit test", function () {
+    it("Init unit test", function (done) {
+        done();
+    });
+
+    after(function (done) {
+        require("./lib/param.js");
+        done();
+    });
+});
