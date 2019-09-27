a00e00f63e269de578ad4780ed45d6ad0f1758cd
antoinehage <me@antoinehage.com>
2019-09-27 13:46:56 -0400

    jshint & license for mw favicon done

--- mw/favicon/index.js
+++ mw/favicon/index.js
@@ -1,6 +1,14 @@
 'use strict';
 
 /**
+ * @license
+ * Copyright SOAJS All Rights Reserved.
+ *
+ * Use of this source code is governed by an Apache license that can be
+ * found in the LICENSE file at the root of this repository
+ */
+
+/**
  *
  * @returns {Function}
  */
@@ -9,8 +17,8 @@ module.exports = () => {
         if (req.url === '/favicon.ico') {
             res.writeHead(200, {'Content-Type': 'image/x-icon'});
             return res.end();
+        } else {
+	        return next();
         }
-        else
-            return next();
     };
 };
