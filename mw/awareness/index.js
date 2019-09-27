7debc7f320b560646898a2d2e688458d12e44bca
antoinehage <me@antoinehage.com>
2019-09-27 12:28:04 -0400

    jshint & license for mw awareness done

--- mw/awareness/index.js
+++ mw/awareness/index.js
@@ -1,32 +1,42 @@
+'use strict';
+
+/**
+ * @license
+ * Copyright SOAJS All Rights Reserved.
+ *
+ * Use of this source code is governed by an Apache license that can be
+ * found in the LICENSE file at the root of this repository
+ */
+
 /**
  *
  * @param param
  * @returns {Function}
  */
 module.exports = {
-    getMw : function(param) {
-	    let driver;
-	    if (param.awareness) {
-		    if (!process.env.SOAJS_DEPLOY_HA) {
-			    driver = require("./custom.js");
-			    driver.init(param);
-		    }
-		    else {
-			    driver = require("./ha.js");
-			    driver.init(param);
-		    }
-	    }
-	    return function (req, res, next) {
-		    if (param.awareness) {
-			    req.soajs.awareness = {
-				    "getHost": driver.getServiceHost
-			    };
-			
-			    if(process.env.SOAJS_DEPLOY_HA){
-				    req.soajs.awareness["getLatestVersionFromCache"] = driver.getLatestVersionFromCache;
-			    }
-		    }
-		    next();
-	    };
-    }
+	getMw: function (param) {
+		let driver;
+		if (param.awareness) {
+			if (!process.env.SOAJS_DEPLOY_HA) {
+				driver = require("./custom.js");
+				driver.init(param);
+			}
+			else {
+				driver = require("./ha.js");
+				driver.init(param);
+			}
+		}
+		return function (req, res, next) {
+			if (param.awareness) {
+				req.soajs.awareness = {
+					"getHost": driver.getServiceHost
+				};
+				
+				if (process.env.SOAJS_DEPLOY_HA) {
+					req.soajs.awareness.getLatestVersionFromCache = driver.getLatestVersionFromCache;
+				}
+			}
+			next();
+		};
+	}
 };
