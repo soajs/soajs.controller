26dca76dda4894047ae8f6dfaf713f5987a2fb53
antoinehage <me@antoinehage.com>
2020-04-13 17:24:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -15,9 +15,9 @@ let config = {
 		"memory": ''
 	},
 	"serviceVersion": "1",
-	"serviceName": "controller",
+	"serviceName": "gateway",
 	"serviceGroup": "SOAJS Core Service",
-	"servicePort": 4000,
+	"servicePort": 3999,
 	"maintenance": {
 		"readiness": "/heartbeat",
 		"port": {"type": "maintenance"},
