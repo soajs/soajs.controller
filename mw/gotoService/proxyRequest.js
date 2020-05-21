6ae2b09c7be59240a4c5e001ee0b37d029fd7e4a
antoinehage <me@antoinehage.com>
2020-05-21 05:38:50 -0400

    added to DELETE body param support

--- mw/gotoService/proxyRequest.js
+++ mw/gotoService/proxyRequest.js
@@ -64,7 +64,7 @@ module.exports = (configuration) => {
 					return req.soajs.controllerResponse(core.error.getError(135));
 				});
 				
-				if (req.method === 'POST' || req.method === 'PUT' || req.method === 'PATCH') {
+				if (req.method === 'POST' || req.method === 'PUT' || req.method === 'PATCH' || req.method === 'DELETE') {
 					req.pipe(req.soajs.controller.redirectedRequest).pipe(res);
 				} else {
 					req.soajs.controller.redirectedRequest.pipe(res);
