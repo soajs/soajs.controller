6158551a0be1b51ce3bac1446244ef92d1b04537
antoinehage <me@antoinehage.com>
2020-07-02 14:14:43 -0400

    removed dependency on registry from infraModule

--- modules/driver/kubernetes/model/lib.js
+++ modules/driver/kubernetes/model/lib.js
@@ -8,59 +8,20 @@
  * found in the LICENSE file at the root of this repository
  */
 
-const registryModule = require("./../../../registry/index.js");
-const get = (p, o) => p.reduce((xs, x) => (xs && xs[x]) ? xs[x] : null, o);
-
 let lib = {
 	"getDriverConfiguration": (soajs, configuration, cb) => {
 		if (!configuration) {
 			return cb(new Error("Problem with the provided kubernetes configuration"));
 		}
-		if (configuration.env) {
-			//get env registry, this must loadByEnv
-			registryModule.loadByEnv({envCode: configuration.env}, (err, envRecord) => {
-				if (err) {
-					if (soajs && soajs.log) {
-						soajs.log.error(err.message);
-					}
-					return cb(new Error("loadByEnv error. Unable to find healthy configuration in registry"));
-				}
-				if (!envRecord) {
-					return cb(new Error("loadByEnv empty. Unable to find healthy configuration in registry"));
-				}
-				let registry = envRecord;
-				let depType = get(["deployer", "type"], registry);
-				let regConf = null;
-				if (depType === "container") {
-					let depSeleted = get(["deployer", "selected"], registry);
-					if (depSeleted && depSeleted.includes("kubernetes")) {
-						regConf = get(["deployer"].concat(depSeleted.split(".")), registry);
-					}
-				}
-				if (regConf) {
-					let protocol = regConf.configuration.protocol || "https";
-					let port = regConf.configuration.port ? ":" + regConf.configuration.port : "";
-					let config = {
-						"namespace": regConf.namespace,
-						"token": regConf.configuration.token,
-						"url": protocol + "://" + regConf.configuration.url + port
-					};
-					return cb(null, config);
-				} else {
-					return cb(new Error("Unable to find healthy configuration in registry"));
-				}
-			});
+		if (configuration.namespace && configuration.token && configuration.url) {
+			let config = {
+				"namespace": configuration.namespace,
+				"token": configuration.token,
+				"url": configuration.url
+			};
+			return cb(null, config);
 		} else {
-			if (configuration.namespace && configuration.token && configuration.url) {
-				let config = {
-					"namespace": configuration.namespace,
-					"token": configuration.token,
-					"url": configuration.url
-				};
-				return cb(null, config);
-			} else {
-				return cb(new Error("Configuration requires namespace, token, and url"));
-			}
+			return cb(new Error("Configuration requires namespace, token, and url"));
 		}
 	},
 	"cleanLabel": (label) => {
--- mw/awareness/ha.js
+++ mw/awareness/ha.js
@@ -8,6 +8,8 @@
  * found in the LICENSE file at the root of this repository
  */
 
+const get = (p, o) => p.reduce((xs, x) => (xs && xs[x]) ? xs[x] : null, o);
+
 const async = require('async');
 const coreLibs = require("soajs.core.libs");
 
@@ -21,6 +23,38 @@ regEnvironment = regEnvironment.toLowerCase();
 let awarenessCache = {};
 
 let lib = {
+	"getInfraToken": (envCode, cb) => {
+		registryModule.loadByEnv({envCode: envCode}, (err, envRecord) => {
+			if (err) {
+				return cb(err);
+			}
+			if (!envRecord) {
+				return cb(new Error("loadByEnv empty. Unable to find healthy configuration in registry"));
+			}
+			let registry = envRecord;
+			let depType = get(["deployer", "type"], registry);
+			let regConf = null;
+			if (depType === "container") {
+				let depSeleted = get(["deployer", "selected"], registry);
+				if (depSeleted && depSeleted.includes("kubernetes")) {
+					regConf = get(["deployer"].concat(depSeleted.split(".")), registry);
+				}
+			}
+			if (regConf) {
+				let protocol = regConf.configuration.protocol || "https";
+				let port = regConf.configuration.port ? ":" + regConf.configuration.port : "";
+				let config = {
+					"namespace": regConf.namespace,
+					"token": regConf.configuration.token,
+					"url": protocol + "://" + regConf.configuration.url + port
+				};
+				return cb(null, config);
+			} else {
+				return cb(new Error("Unable to find healthy configuration in registry"));
+			}
+		});
+	},
+	
 	"getHostFromCache": function (serviceName, version) {
 		if (awarenessCache[serviceName] &&
 			awarenessCache[serviceName][version] &&
@@ -34,96 +68,114 @@ let lib = {
 	
 	"getHostFromAPI": function (serviceName, version, cb) {
 		if (!version) {
-			let options = null;
-			let input = {
-				"configuration": {"env": regEnvironment},
-				"itemName": serviceName
-			};
-			param.infra.kubernetes.get.item_latestVersion(param, input, options, (error, obtainedVersion) => {
+			lib.getInfraToken(regEnvironment, (error, configuration) => {
 				if (error) {
 					param.log.error(error.message);
 					return cb(null);
 				}
-				getHost(obtainedVersion);
+				let options = null;
+				let input = {
+					"configuration": {"configuration": configuration},
+					"itemName": serviceName
+				};
+				param.infraModule.kubernetes.get.item_latestVersion(param, input, options, (error, obtainedVersion) => {
+					if (error) {
+						param.log.error(error.message);
+						return cb(null);
+					}
+					getHost(obtainedVersion);
+				});
 			});
 		} else {
 			getHost(version);
 		}
 		
 		function getHost(version) {
-			param.log.debug('Getting host for ' + serviceName + ' - ' + version + ' ....');
-			let options = null;
-			let input = {
-				"configuration": {"env": regEnvironment},
-				"item": {"name": serviceName, "version": version, "env": regEnvironment}
-			};
-			param.infra.kubernetes.get.host(param, input, options, (error, host) => {
+			lib.getInfraToken(regEnvironment, (error, configuration) => {
 				if (error) {
 					param.log.error(error.message);
 					return cb(null);
 				}
-				if (!awarenessCache[serviceName]) {
-					awarenessCache[serviceName] = {};
-				}
-				if (!awarenessCache[serviceName][version]) {
-					awarenessCache[serviceName][version] = {};
-				}
-				awarenessCache[serviceName][version].host = host;
-				return cb(host);
+				param.log.debug('Getting host for ' + serviceName + ' - ' + version + ' ....');
+				let options = null;
+				let input = {
+					"configuration": {"configuration": configuration},
+					"item": {"name": serviceName, "version": version, "env": regEnvironment}
+				};
+				param.infraModule.kubernetes.get.host(param, input, options, (error, host) => {
+					if (error) {
+						param.log.error(error.message);
+						return cb(null);
+					}
+					if (!awarenessCache[serviceName]) {
+						awarenessCache[serviceName] = {};
+					}
+					if (!awarenessCache[serviceName][version]) {
+						awarenessCache[serviceName][version] = {};
+					}
+					awarenessCache[serviceName][version].host = host;
+					return cb(host);
+				});
 			});
 		}
 	},
 	
 	"rebuildAwarenessCache": function () {
-		let myCache = {};
-		let options = null;
-		let input = {
-			"configuration": {"env": regEnvironment}
-		};
-		param.infra.kubernetes.get.all(param, input, options, (error, services) => {
+		lib.getInfraToken(regEnvironment, (error, configuration) => {
 			if (error) {
 				param.log.error(error.message);
-				return;
+				return cb(null);
 			}
-			async.each(services, function (oneService, callback) {
-				let version, serviceName;
-				if (oneService.metadata.labels['soajs.service.type'] !== "service" && oneService.metadata.labels['soajs.service.type'] !== "mdaemon") {
+			let myCache = {};
+			let options = null;
+			let input = {
+				"configuration": {"configuration": configuration},
+			};
+			param.infraModule.kubernetes.get.all(param, input, options, (error, services) => {
+				if (error) {
+					param.log.error(error.message);
+					return;
+				}
+				async.each(services, function (oneService, callback) {
+					let version, serviceName;
+					if (oneService.metadata.labels['soajs.service.type'] !== "service" && oneService.metadata.labels['soajs.service.type'] !== "mdaemon") {
+						return callback();
+					}
+					if (oneService.metadata.labels && oneService.metadata.labels['soajs.service.version']) {
+						version = oneService.metadata.labels['soajs.service.version'];
+					}
+					
+					if (oneService.metadata.labels && oneService.metadata.labels['soajs.service.name']) {
+						serviceName = oneService.metadata.labels['soajs.service.name'];
+					}
+					
+					if (!serviceName) {
+						return callback();
+					}
+					if (oneService.spec.clusterIP) {
+						if (!myCache[serviceName]) {
+							myCache[serviceName] = {};
+						}
+						if (!myCache[serviceName][version]) {
+							myCache[serviceName][version] = {};
+						}
+						myCache[serviceName][version].host = oneService.spec.clusterIP;
+					}
 					return callback();
-				}
-				if (oneService.metadata.labels && oneService.metadata.labels['soajs.service.version']) {
-					version = oneService.metadata.labels['soajs.service.version'];
-				}
-				
-				if (oneService.metadata.labels && oneService.metadata.labels['soajs.service.name']) {
-					serviceName = oneService.metadata.labels['soajs.service.name'];
-				}
-				
-				if (!serviceName) {
-					return callback();
-				}
-				if (oneService.spec.clusterIP) {
-					if (!myCache[serviceName]) {
-						myCache[serviceName] = {};
+				}, function () {
+					awarenessCache = myCache;
+					param.log.debug("Awareness cache rebuilt successfully");
+					param.log.debug(awarenessCache);
+					
+					let cacheTTL = registryModule.get().serviceConfig.awareness.cacheTTL;
+					if (cacheTTL) {
+						if (timeout) {
+							clearTimeout(timeout);
+						}
+						param.log.debug("rebuilding cache in: " + cacheTTL);
+						timeout = setTimeout(lib.rebuildAwarenessCache, cacheTTL);
 					}
-					if (!myCache[serviceName][version]) {
-						myCache[serviceName][version] = {};
-					}
-					myCache[serviceName][version].host = oneService.spec.clusterIP;
-				}
-				return callback();
-			}, function () {
-				awarenessCache = myCache;
-				param.log.debug("Awareness cache rebuilt successfully");
-				param.log.debug(awarenessCache);
-				
-				let cacheTTL = registryModule.get().serviceConfig.awareness.cacheTTL;
-				if (cacheTTL) {
-					if (timeout) {
-						clearTimeout(timeout);
-					}
-					param.log.debug("rebuilding cache in: " + cacheTTL);
-					timeout = setTimeout(lib.rebuildAwarenessCache, cacheTTL);
-				}
+				});
 			});
 		});
 	}
@@ -202,17 +254,23 @@ let ha = {
 	},
 	
 	"getLatestVersionFromCluster": function (name, cb) {
-		let options = null;
-		let input = {
-			"configuration": {"env": regEnvironment},
-			"itemName": name
-		};
-		param.infra.kubernetes.get.item_latestVersion(param, input, options, (error, obtainedVersion) => {
+		lib.getInfraToken(regEnvironment, (error, configuration) => {
 			if (error) {
 				param.log.error(error.message);
 				return cb(null);
 			}
-			return cb(obtainedVersion);
+			let options = null;
+			let input = {
+				"configuration": {"configuration": configuration},
+				"itemName": name
+			};
+			param.infraModule.kubernetes.get.item_latestVersion(param, input, options, (error, obtainedVersion) => {
+				if (error) {
+					param.log.error(error.message);
+					return cb(null);
+				}
+				return cb(obtainedVersion);
+			});
 		});
 	}
 };
