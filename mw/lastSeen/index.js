3e84aebfaf3263d9cf507a4e09f8dde687e66221
antoinehage <me@antoinehage.com>
2025-03-19 14:01:28 -0400

    skipping lastseen if no uracDriver

--- mw/lastSeen/index.js
+++ mw/lastSeen/index.js
@@ -21,92 +21,91 @@ module.exports = () => {
     return (req, res, next) => {
         next();
 
-        let lastSeen = get(["soajs", "registry", "custom", "gateway", "value", "lastSeen"], req);
-        if (lastSeen && lastSeen.active) {
+        if (req.soajs.uracDriver) {
+            let lastSeen = get(["soajs", "registry", "custom", "gateway", "value", "lastSeen"], req);
+            if (lastSeen && lastSeen.active) {
 
-            const item = {
-                "name": lastSeen.serviceName || "urac",
-                "version": lastSeen.serviceVersion || "3",
-                "api": lastSeen.api || "/user/last/seen"
-            };
+                const item = {
+                    "name": lastSeen.serviceName || "urac",
+                    "version": lastSeen.serviceVersion || "3",
+                    "api": lastSeen.api || "/user/last/seen"
+                };
 
-            let userId = null;
-            if (req.soajs.uracDriver) {
+                let userId = null;
                 let uracObj = req.soajs.uracDriver.getProfile();
                 if (uracObj) {
                     userId = uracObj._id;
                 }
-            }
+                if (userId) {
+                    req.soajs.awareness.getHost(item.name, item.version, (host) => {
+                        if (host) {
+                            item.host = host;
+                            item.port = req.soajs.registry.services[item.name].port;
 
-            if (userId) {
-                req.soajs.awareness.getHost(item.name, item.version, (host) => {
-                    if (host) {
-                        item.host = host;
-                        item.port = req.soajs.registry.services[item.name].port;
-
-                        let options = {
-                            "uri": "http://" + item.host + ":" + item.port + item.api,
-                            "json": true,
-                            "method": "POST"
-                        };
-                        if (req.headers && req.headers.soajsinjectobj) {
-                            options.headers = { "soajsinjectobj": req.headers.soajsinjectobj };
-                        }
-                        httpRequest(options)
-                            .then(() => { })
-                            .catch(({ error, body }) => {
-                                if (error) {
-                                    req.soajs.log.debug("lastSeen failed for [" + item.name + "@" + item.version + " " + item.api + "]");
-                                    req.soajs.log.debug("lastSeen " + error.message);
-                                } else if (!body.result) {
-                                    let errorMsg = "";
-                                    if (body.errors && body.errors && body.errors.details && body.errors.details.length > 0) {
-                                        body.errors.details.forEach((detail) => {
-                                            if (errorMsg === "") {
-                                                errorMsg += detail.message;
-                                            }
-                                            else {
-                                                errorMsg += " - " + detail.message;
-                                            }
-                                        });
+                            let options = {
+                                "uri": "http://" + item.host + ":" + item.port + item.api,
+                                "json": true,
+                                "method": "POST"
+                            };
+                            if (req.headers && req.headers.soajsinjectobj) {
+                                options.headers = { "soajsinjectobj": req.headers.soajsinjectobj };
+                            }
+                            httpRequest(options)
+                                .then(() => { })
+                                .catch(({ error, body }) => {
+                                    if (error) {
+                                        req.soajs.log.debug("lastSeen failed for [" + item.name + "@" + item.version + " " + item.api + "]");
+                                        req.soajs.log.debug("lastSeen " + error.message);
+                                    } else if (!body.result) {
+                                        let errorMsg = "";
+                                        if (body.errors && body.errors && body.errors.details && body.errors.details.length > 0) {
+                                            body.errors.details.forEach((detail) => {
+                                                if (errorMsg === "") {
+                                                    errorMsg += detail.message;
+                                                }
+                                                else {
+                                                    errorMsg += " - " + detail.message;
+                                                }
+                                            });
+                                        }
+                                        req.soajs.log.debug("lastSeen", errorMsg);
                                     }
-                                    req.soajs.log.debug("lastSeen", errorMsg);
-                                }
-                            });
+                                });
 
-                        // let options = {
-                        //     method: "post",
-                        //     uri: "http://" + item.host + ":" + item.port + item.api,
-                        //     json: true
-                        // };
-                        // if (req.headers && req.headers.soajsinjectobj) {
-                        //     options.headers = { "soajsinjectobj": req.headers.soajsinjectobj };
-                        // }
-                        // request(options, (error, response, body) => {
-                        //     if (error) {
-                        //         req.soajs.log.debug("lastSeen failed for [" + item.name + "@" + item.version + " " + item.api + "]");
-                        //         req.soajs.log.debug("lastSeen " + error.message);
-                        //     } else if (!body.result) {
-                        //         let errorMsg = "";
-                        //         if (body.errors && body.errors && body.errors.details && body.errors.details.length > 0) {
-                        //             body.errors.details.forEach((detail) => {
-                        //                 if (errorMsg === "") {
-                        //                     errorMsg += detail.message;
-                        //                 }
-                        //                 else {
-                        //                     errorMsg += " - " + detail.message;
-                        //                 }
-                        //             });
-                        //         }
-                        //         req.soajs.log.debug("lastSeen", errorMsg);
-                        //     }
-                        // });
-                    } else {
-                        req.soajs.log.debug("lastSeen failed with no host for [" + item.name + "@" + item.version + " " + item.api + "]");
-                    }
-                });
-            } else {
-                req.soajs.log.debug("lastSeen failed with no userId for [" + item.name + "@" + item.version + " " + item.api + "]");
+                            // let options = {
+                            //     method: "post",
+                            //     uri: "http://" + item.host + ":" + item.port + item.api,
+                            //     json: true
+                            // };
+                            // if (req.headers && req.headers.soajsinjectobj) {
+                            //     options.headers = { "soajsinjectobj": req.headers.soajsinjectobj };
+                            // }
+                            // request(options, (error, response, body) => {
+                            //     if (error) {
+                            //         req.soajs.log.debug("lastSeen failed for [" + item.name + "@" + item.version + " " + item.api + "]");
+                            //         req.soajs.log.debug("lastSeen " + error.message);
+                            //     } else if (!body.result) {
+                            //         let errorMsg = "";
+                            //         if (body.errors && body.errors && body.errors.details && body.errors.details.length > 0) {
+                            //             body.errors.details.forEach((detail) => {
+                            //                 if (errorMsg === "") {
+                            //                     errorMsg += detail.message;
+                            //                 }
+                            //                 else {
+                            //                     errorMsg += " - " + detail.message;
+                            //                 }
+                            //             });
+                            //         }
+                            //         req.soajs.log.debug("lastSeen", errorMsg);
+                            //     }
+                            // });
+                        } else {
+                            req.soajs.log.debug("lastSeen failed with no host for [" + item.name + "@" + item.version + " " + item.api + "]");
+                        }
+                    });
+                } else {
+                    req.soajs.log.debug("lastSeen failed with no userId for [" + item.name + "@" + item.version + " " + item.api + "]");
+                }
             }
         }
     };
